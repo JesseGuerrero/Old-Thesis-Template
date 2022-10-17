089e3e06f22ad3b84b8407e6e16faa23a579991a
Jesse Guerrero <jesseguerrero1991@gmail.com>
2022-10-16 21:31:59 -0500

    Updated with testing caption files

--- Adversarial Learning/human_from_coco/reparseCOCO.py
+++ Adversarial Learning/human_from_coco/reparseCOCO.py
@@ -2,16 +2,22 @@ import json
 
 jsonFile = open('captions_train2017.json')
 jsonData = json.load(jsonFile)
-outData = open('humanCOCO.txt','w')
+outData = open('humanCOCOTest.txt','w')
 
 captionDict = {}
 for i, caption in enumerate(jsonData['annotations']):
-    captionDict["000000" + str(caption['image_id'])] = []
+    if(caption['image_id'] < 10_000):
+        captionDict[caption['image_id']] = []
 for i, caption in enumerate(jsonData['annotations']):
-    captionDict["000000" + str(caption['image_id'])].append(caption['caption'])
+    if(caption['image_id'] < 10_000):
+        captionDict[caption['image_id']].append(caption['caption'])
 # print(captionDict)
-for key, value in captionDict.items():
-    sentence = " ".join(value)
+captionkeys = []
+for key in captionDict.keys():
+    captionkeys.append(key)
+captionkeys.sort()
+for key in captionkeys:
+    sentence = " ".join(captionDict[key])
     sentence = sentence.replace("\n", "").replace(",", "").replace(".", "").replace("'", "").lower()
     outData.write(str(key) + ":" + str(sentence) + "\n")
 
